707315f334e1c6364b6bc1652a7fc87f0c18ce8e
nursultanyerzhan <78619852+nursultanyerzhan@users.noreply.github.com>
2025-11-10 10:40:29 +0500

    +1

--- Controllers/Authorization.cs
+++ Controllers/Authorization.cs
@@ -132,13 +132,4 @@ namespace AuthorizationsSso.Controllers
         public long expires_in { get; set; }
         public long expires { get; set; }
     }
-
-    public class DtoAuthorization
-    {
-        public string code { get; set; }
-        public string client_id { get; set; }
-        public string client_secret { get; set; }
-        public string redirect_uri { get; set; }
-        public string grant_type { get; set; }
-    }
 }
